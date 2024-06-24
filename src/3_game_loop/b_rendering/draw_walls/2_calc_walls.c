1fc46dcdbe4b3987a1a4cbd9b20b188277d69211
FreddyMSchubert <github@frederickschubert.de>
2024-06-24 10:23:38 +0200

    minor fisheye correction adjustments

--- src/3_game_loop/b_rendering/draw_walls/2_calc_walls.c
+++ src/3_game_loop/b_rendering/draw_walls/2_calc_walls.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   2_calc_walls.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: freddy <freddy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 1999/06/13 09:10:15 by fschuber          #+#    #+#             */
-/*   Updated: 2024/06/24 10:14:02 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/06/24 10:21:41 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ static inline int	get_height_from_intersection_dist(double corrected_dist)
 	int		height;
 	double	vertical_fov_rad;
 
-	vertical_fov_rad = 2 * atan(tan(rad_to_deg(FOV_DEG) / 2)
+	vertical_fov_rad = 4.1f * atan(tan(rad_to_deg(FOV_DEG) / 2)
 			/ (game()->mlx->width / game()->mlx->height));
 	height = (game()->mlx->height) / (tan(vertical_fov_rad) * corrected_dist);
 	return (abs(height));
@@ -65,8 +65,8 @@ void	calc_gameobject(int ray_index, t_vec2 intersect)
 	get_x_pixel_from_ray_index(ray_index, &start_x, &end_x);
 	intersection_dist = pos_distance(player()->transform.pos, intersect);
 	angle_correction_rad = deg_to_rad(get_fisheye_corrected_ray_angle(ray_index));
-	corrected_dist = intersection_dist * cos(angle_correction_rad);	// Correct the distance
-	height = get_height_from_intersection_dist(corrected_dist);	// Use the corrected distance
+	corrected_dist = intersection_dist * cos(angle_correction_rad);
+	height = get_height_from_intersection_dist(corrected_dist);
 	hit_offset = intersect.y - floor(intersect.y) + \
 						intersect.x - floor(intersect.x);
 	draw_gameobject(start_x, end_x, height, \
